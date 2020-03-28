fecf106965fa88b8b111bf474f798d62b88764fc
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2020-03-28 19:28:09 +0100

    Adding all the generated code since openapi-generator cannot run as part of build

---
+++ confidentify_client/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from confidentify_client.api.accounts_api import AccountsApi
+from confidentify_client.api.auth_api import AuthApi
+from confidentify_client.api.datasets_api import DatasetsApi
+from confidentify_client.api.matching_api import MatchingApi
+from confidentify_client.api.process_api import ProcessApi
